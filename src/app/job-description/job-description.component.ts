ef078022b5b20808c4cc869729c971e5f0aaa760
Krishan <117340271+shoonya0@users.noreply.github.com>
2023-10-17 23:54:54 +0530

    job description file created and start working on it

--- src/app/job-description/job-description.component.ts
+++ src/app/job-description/job-description.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-job-description',
   templateUrl: './job-description.component.html',
   styleUrls: ['./job-description.component.css']
 })
-export class JobDescriptionComponent {
+export class JobDescriptionComponent implements OnInit{
+
+  user:any;
+
+  constructor(){ 
+    this.user={
+      name:"krishan",
+      tittle:"SDE-2",
+      address:"no body knows",
+      phone:[
+        '21212113',
+        '32443232'
+      ]
+    };
+  }
+
+  ngOnInit(){ }
+
 
 }
